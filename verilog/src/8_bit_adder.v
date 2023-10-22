b36be1be418a9c45e09787e1d2625e0ee2ce3bf4
Mori Adrian Rosland <mori@live.no>
2023-10-22 14:01:06 +0200

    Refactored 8-bit adder

--- verilog/src/8_bit_adder.v
+++ verilog/src/8_bit_adder.v
@@ -17,17 +17,19 @@ module full_adder(a, b, carry_in, sum, carry_out);
     or or1(carry_out, w2, w3);
 endmodule
 
-module adder_8_bit (a0, a1, a2, a3, a4, a5, a6, a7, b0, b1, b2, b3, b4, b5, b6, b7, sum0, sum1, sum3, sum4, sum5, sum6, sum7);
-    input a0, a1, a2, a3, a4, a5, a6, a7, b0, b1, b2, b3, b4, b5, b6, b7;
-    output sum0, sum1, sum3, sum4, sum5, sum6, sum7;
+module adder_8_bit (
+    input [7:0] a,
+    input [7:0] b,
+    output [7:0] sum
+);
 
     wire w1, w2, w3, w4, w5, w6, w7;
 
-    half_adder half_adder1(a0, b0, sum0, w1);
-    full_adder full_adder1(a1, b1, w1, sum1, w2);
-    full_adder full_adder2(a2, b2, w2, sum2, w3);
-    full_adder full_adder3(a3, b3, w3, sum3, w4);
-    full_adder full_adder4(a4, b4, w4, sum4, w5);
-    full_adder full_adder5(a5, b5, w5, sum5, w6);
-    full_adder full_adder6(a6, b6, w6, sum6, w7);
+    half_adder half_adder1(a[0], b[0], sum[0], w1);
+    full_adder full_adder1(a[1], b[1], w1, sum[1], w2);
+    full_adder full_adder2(a[2], b[2], w2, sum[2], w3);
+    full_adder full_adder3(a[3], b[3], w3, sum[3], w4);
+    full_adder full_adder4(a[4], b[4], w4, sum[4], w5);
+    full_adder full_adder5(a[5], b[5], w5, sum[5], w6);
+    full_adder full_adder6(a[6], b[6], w6, sum[6], w7);
 endmodule
