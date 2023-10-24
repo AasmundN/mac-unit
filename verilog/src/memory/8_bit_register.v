42abeeb077f53f80a3cc59dcc317ab6202580860
Mori Adrian Rosland <mori@live.no>
2023-10-24 13:52:47 +0200

    Defined 8-bit register headers

--- verilog/src/memory/8_bit_register.v
+++ verilog/src/memory/8_bit_register.v
@@ -1,12 +1,7 @@
 `include "1_bit_register.v"
 
-module eight_bit_register (
-    output [7:0] out,
-    input [7:0] in,
-    input enable,
-    input reset,
-    input clk);
+module eight_bit_register (output [7:0] out,input [7:0] in, input enable, input reset, input clk);
 
+    // 8-bit register...
     
-
 endmodule
