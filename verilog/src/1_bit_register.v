bdf87533f88f50661b0a71bdb79893ea0d15855f
moriar <moriar@stud.ntnu.no>
2023-10-24 11:40:06 +0200

    Added 1-bit register

--- verilog/src/1_bit_register.v
+++ verilog/src/1_bit_register.v
@@ -1,13 +1,13 @@
-`include "d_flip_flop.v"
-`include "2_to_1_mux.v"
+`include "../src/d_flip_flop.v"
+`include "../src/2_to_1_mux.v"
 
 module one_bit_register (output out, input in, input enable, input reset, input clk);
     wire w0, w1, w3;
 
-    two_to_one_mux mux0 (w0, in, q, enable);
+    two_to_one_mux mux0 (w0, in, out, enable);
 
     not not0 (w1, reset);
     and and0 (w3, w0, w1);
 
-    d_flip_flop dff0(q, w3, clk);
+    d_flip_flop d_flip_flop0(out, w3, clk);
 endmodule
