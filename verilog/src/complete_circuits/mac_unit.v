97391276aae9ef54d395f4baa7a2cd2a93ff2cc3
Mori Adrian Rosland <mori@live.no>
2023-10-31 16:48:33 +0100

    Fixed undefined adder outputs

--- verilog/src/complete_circuits/mac_unit.v
+++ verilog/src/complete_circuits/mac_unit.v
@@ -2,14 +2,14 @@
 `include "../../src/memory/8_bit_register.v"
 `include "../../src/multiplier/two_bit_multiplier.v"
 
-module mac_unit (output [7:0] out, input [1:0] a, input [1:0] b, input enable, input reset, input clk);
+module mac_unit (output [7:0] out, input [1:0] a, input [1:0] b, input [3:0] zero_input, input enable, input reset, input clk);
 
-    wire [7:0] mult_out;
+    wire [3:0] mult_out;
     wire [7:0] add_out;
     wire carry_out;
 
-    two_bit_multiplier mult0 (a,b, mult_out[3:0]);
-    adder_8_bit adder0 (mult_out, out, add_out, carry_out);
+    two_bit_multiplier mult0 (a,b, mult_out);
+    adder_8_bit adder0 (mult_out, zero_input, out, add_out, carry_out);
     eight_bit_register reg0 (out, add_out, enable, reset, clk);
 
 endmodule
